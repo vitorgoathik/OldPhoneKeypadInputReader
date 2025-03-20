ee2d1eebe3834deaee90e17bae2c78fc130c9b45
vitorgoathik <vitor.goathik@gmail.com>
2025-03-20 20:13:37 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+public class OldPhonePad
+{
+    // Define the mappings for each number to corresponding letters
+    private static readonly string[] keyMappings = new string[]
+    {
+        "",    // when user presses 0
+        "",    // when user presse 1
+        "ABC", // 2
+        "DEF", // 3
+        "GHI", // 4
+        "JKL", // 5
+        "MNO", // 6
+        "PQRS",// 7
+        "TUV", // 8
+        "WXYZ" // 9
+    };
+
+    public static string OldPhonePadMethod(string input)
+    {
+        StringBuilder result = new StringBuilder();
+        StringBuilder currentInput = new StringBuilder();
+
+        input = input.TrimEnd('#');
+
+        return result.ToString();
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(OldPhonePadMethod("33#"));
+    }
+}
